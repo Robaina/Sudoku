1a2a9f4501dee2d8221ce3be4d6cd7cdc914bfc1
Semidán Robaina-Estévez <tintanjarafe@gmail.com>
2019-01-21 22:18:24 +0000

    prototype

--- lp.js
+++ lp.js
@@ -1,33 +1,115 @@
-// Build LP model
-// let ncols = nrows = 2;
-// let model;
-//
-// function addSquareConstraints(constraint_array, n=2) {
-//
-//     for (let i=0; i<n**4 - 1; i+=n**2) {
-//         for (let j=1; j<=n**2; j++) {
-//             constraint_str = "";
-//             for (let k=0; k<n**2; k++) {
-//                 constraint_str += "x" + (i + k) + j + " ";
-//             }
-//             constraint_str += "= 1";
-//             constraint_array.push(constraint_str);
-//         }
-//     }
-//     return constraint_array
-// }
-//
-//
-// model = [
-//           "max: 1200 table 1600 dresser",
-//           "30 table 20 dresser <= 300",
-//           "5 table 10 dresser <= 110",
-//           "30 table 50 dresser <= 400",
-//           "int table",
-//           "int dresser",
-//         ];
-//
-//
-// // Reformat to JSON model
-// model = solver.ReformatLP(model);
-// let result = solver.Solve(model);
+// construct model string
+function get2DCoordinates(n, width) {
+  let j = n % width;
+  let i = Math.floor(n / width);
+  return {"x": i, "y": j}
+}
+
+function addSquareConstraints() {
+    let c_str = "";
+    for (let i=1; i<=nrows**2; i++) {
+        for (let n=0; n<nrows*ncols; n++) {
+            let bigcoor = get2DCoordinates(n, ncols);
+                for (let m=0; m<nrows*ncols; m++) {
+                    let smallcoor = get2DCoordinates(m, ncols);
+
+                    c_str += "+x" + (nrows*bigcoor.x + smallcoor.x) + (ncols*bigcoor.y + smallcoor.y) + i + " ";
+                }
+                c_str += "= 1\n";
+        }
+    }
+    return c_str
+}
+
+function addRowAndColumnConstraints() {
+    let row_str, col_str;
+    let c_str = "";
+    for (let i=1; i<=nrows**2; i++) {
+        for (let n=0; n<nrows**2; n++) {
+            row_str = col_str = "";
+            for(let m=0; m<ncols**2; m++) {
+                row_str += "+x" + n + m + i + " ";
+                col_str += "+x" + m + n + i + " ";
+            }
+            row_str += "= 1\n";
+            col_str += "= 1\n";
+            c_str += row_str + col_str;
+        }
+    }
+    return c_str
+}
+
+function addUniquenessConstraints() {
+    let c_str = "";
+    for (let n=0; n<nrows**2; n++) {
+        for(let m=0; m<ncols**2; m++) {
+            for (let i=1; i<=nrows**2; i++) {
+                c_str += "+x" + n + m + i + " ";
+            }
+            c_str += "= 1\n";
+        }
+    }
+    return c_str
+}
+
+function writeConstraints() {
+    let constraints_str = "\nSubject To\n";
+    constraints_str += addSquareConstraints() + addRowAndColumnConstraints()
+        + addUniquenessConstraints();
+    return constraints_str
+}
+
+function writeBinaryVariables() {
+  let b_str = "\nGeneral\n";
+  for (let n=0; n<nrows**2; n++) {
+      for(let m=0; m<ncols**2; m++) {
+          for (let i=1; i<=nrows**2; i++) {
+              b_str += "x" + n + m + i + " ";
+          }
+      }
+  }
+  return b_str
+}
+
+function addInitialCondition() {
+  // Allows to specify a number for a given square. This can be used to solve
+  // a Sudoku problem.
+  let c_str = "";
+  for (selectedCell of selectedCells) {
+    c_str += selectedCell + " = 1\n";
+  }
+  return c_str
+}
+
+// let model_str = "";
+let solution = {};
+let value;
+// let nrows = ncols = 2;
+
+function solveMILP() {
+  let model_str = "";
+  removeSelected();
+  // Reformat programs to glpk.js
+  model_str += "Minimize\n+x001\n" + writeConstraints() + addInitialCondition() + writeBinaryVariables() + "\nEnd";
+
+  // Call glpk solver and solve model
+  let lp = glp_create_prob();
+  glp_read_lp_from_string(lp, null, model_str);
+
+  glp_scale_prob(lp, GLP_SF_AUTO);
+
+  let smcp = new SMCP({presolve: GLP_ON});
+  glp_simplex(lp, smcp);
+
+  let iocp = new IOCP({presolve: GLP_ON});
+  glp_intopt(lp, iocp);
+
+  for (let i=1; i<=glp_get_num_cols(lp); i++){
+    value = Math.round(glp_mip_col_val(lp, i));
+    if (value === 1) {
+      solution[glp_get_col_name(lp, i)] = value;
+    }
+  }
+  fillGrid();
+  return solution
+}
--- sudoku.js
+++ sudoku.js
@@ -1,22 +1,37 @@
-let ncols = nrows = 2;
-let constraint_array = [];
+let ncols = nrows = 3;
 let selectedCell;
+let selectedCells = ["x012", "x053", "x084",
+                     "x108", "x137", "x163",
+                     "x179", "x227", "x276",
+                     "x281", "x323", "x347",
+                     "x388", "x426", "x452",
+                     "x461", "x473", "x505",
+                     "x614", "x638", "x686",
+                     "x728", "x741", "x772",
+                     "x815", "x859"];
+let selectedCellsNumbers = [];
 
-// Build array LP model
-constraint_array.push("max: ");
-constraint_array = addSquareConstraints(constraint_array);
-constraint_array = addRowAndColumnConstraints(constraint_array);
-constraint_array = addUniquenessConstraints(constraint_array);
 
-// Reformat to JSON model
-model = solver.ReformatLP(constraint_array);
+function fillGrid() {
+  let grid_numbers = getGridNumbers(solution);
 
-// Solve the model
-let sudoku = solver.Solve(model);
-let grid_numbers = getGridNumbers();
+  for (let n=0; n<nrows*ncols; n++) {
+
+    let bigcoor = get2DCoordinates(n, ncols);
+    for (let j=0; j<nrows*ncols; j++) {
+
+      let smallcoor = get2DCoordinates(j, ncols);
+      let xcoor = (nrows*bigcoor.x + smallcoor.x);
+      let ycoor = (ncols*bigcoor.y + smallcoor.y);
+      let smallCell = document.getElementById("x" + xcoor + ycoor);
+
+      smallCell.style.color = "rgb(240, 239, 239)";
+      smallCell.innerHTML = grid_numbers["" + xcoor + ycoor];
+    }
+  }
+}
 
 function displayGrid() {
-
   let gameGrid = document.getElementById("game_grid");
   gameGrid.style.setProperty("grid-template-columns",
     "repeat(" + ncols + ", auto)");
@@ -39,11 +54,11 @@ function displayGrid() {
       let xcoor = (nrows*bigcoor.x + smallcoor.x);
       let ycoor = (ncols*bigcoor.y + smallcoor.y);
 
-      smallcell.setAttribute("id", "n" + xcoor + ycoor);
+      smallcell.setAttribute("id", "x" + xcoor + ycoor);
 
       smallcell.addEventListener("click", selectCell);
       smallcell.addEventListener("touch", selectCell);
-      smallcell.innerHTML = grid_numbers["" + xcoor + ycoor];
+      smallcell.innerHTML = "-";
       bigcell.appendChild(smallcell);
     }
 
@@ -52,13 +67,13 @@ function displayGrid() {
 
 }
 
-function getGridNumbers() {
+function getGridNumbers(solution) {
   let grid_numbers = {};
-  let entries = Object.entries(sudoku).slice(3);
+  let entries = Object.entries(solution);
 
   for (entry of entries) {
 
-    if (Math.round(entry[1]) === 1) {
+    if (entry[1] === 1) {
       let id = entry[0].slice(1, 3);
       let value = parseInt(entry[0].slice(3));
       grid_numbers[id] = value;
@@ -73,72 +88,33 @@ function get2DCoordinates(n, width) {
   return {"x": i, "y": j}
 }
 
-
-function selectNumber() {
-  let input = document.getElementById("input-number");
-  let number = input.value;
-  selectedCell.innerHTML = number.toString();
-  selectedCell.classList.remove("selected-cell");
-}
-
 function selectCell(event) {
   selectedCell = event.target;
-  console.log(selectedCell.id);
+  removeSelected();
   selectedCell.classList.add("selected-cell");
+  selectedCell.style.color = "rgb(240, 239, 239)";
 }
 
-function get2DCoordinates(n, width) {
-  let j = n % width;
-  let i = Math.floor(n / width);
-  return {"x": i, "y": j}
+function getCellNumber(number) {
+  selectedCell.innerHTML = number.toString();
+  selectedCell.classList.remove("selected-cell");
+  // selectedCell.classList.add("filled-cell");
+  selectedCell.style.color = "rgb(240, 239, 239)";
+  selectedCells.push(selectedCell.id + number);
 }
 
-function addSquareConstraints(constraint_array) {
-    let constraint_str;
-    for (let i=1; i<=nrows**2; i++) {
-        for (let n=0; n<nrows*ncols; n++) {
-            let bigcoor = get2DCoordinates(n, ncols);
-                constraint_str = "";
-                for (let m=0; m<nrows*ncols; m++) {
-                    let smallcoor = get2DCoordinates(m, ncols);
-
-                    constraint_str += "x" + (nrows*bigcoor.x + smallcoor.x) + (ncols*bigcoor.y + smallcoor.y) + i + " ";
-                }
-                constraint_str += "= 1";
-                constraint_array.push(constraint_str);
-        }
-    }
-    return constraint_array
+function removeSelected() {
+  let cells = document.getElementsByClassName("small-cell");
+  for (let i=0; i<cells.length; i++) {
+    cells[i].classList.remove("selected-cell");
+  }
 }
 
-function addRowAndColumnConstraints(constraint_array) {
-    let row_str, col_str;
-    for (let i=1; i<=nrows**2; i++) {
-        for (let n=0; n<nrows**2; n++) {
-            row_str = col_str = "";
-            for(let m=0; m<ncols**2; m++) {
-                row_str += "x" + n + m + i + " ";
-                col_str += "x" + m + n + i + " ";
-            }
-            row_str += "= 1";
-            col_str += "= 1";
-            constraint_array.push(row_str, col_str);
-        }
-    }
-    return constraint_array
-}
-
-function addUniquenessConstraints(constraint_array) {
-    let constraint_str;
-    for (let n=0; n<nrows**2; n++) {
-        for(let m=0; m<ncols**2; m++) {
-            constraint_str = "";
-            for (let i=1; i<=nrows**2; i++) {
-                constraint_str += "x" + n + m + i + " ";
-            }
-            constraint_str += "= 1";
-            constraint_array.push(constraint_str);
-        }
-    }
-    return constraint_array
+function reset() {
+  selectedCells = [];
+  let cells = document.getElementsByClassName("small-cell");
+  for (let i=0; i<cells.length; i++) {
+    cells[i].innerHTML = "-";
+    cells[i].style.color = "rgba(240, 239, 239, 0)";
+  }
 }
